a86a2949d4381c3b992889a08eeb03d25c13b8cf
brickatyourfeet <brickatyourfeet@gmail.com>
2017-09-24 08:26:08 -0700

    add binary string add and subtract

---
+++ binary-string.js
@@ -0,0 +1,18 @@
+function binaryAdd(a, b) {
+  var apples = parseInt(a, 2)
+  var bananas = parseInt(b, 2)
+  //console.log(apples + bananas)
+  var basket = apples + bananas
+  return basket.toString(2)
+  console.log(basket.toString(2))
+}
+
+
+function binarySubtract(a, b) {
+  var apples = parseInt(a, 2)
+  var bananas = parseInt(b, 2)
+  //console.log(apples + bananas)
+  var basket = apples - bananas
+  return basket.toString(2)
+  console.log(basket.toString(2))
+}
