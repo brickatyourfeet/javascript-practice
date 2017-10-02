32f966ba17f31c451a0d7f366bc9fa8d1b09bae8
brickatyourfeet <brickatyourfeet@gmail.com>
2017-10-02 15:10:18 -0700

    add more map and filter functions

--- hof-pt3.js
+++ hof-pt3.js
@@ -4,7 +4,8 @@ var bugs = [
     'beetle',
     'worm',
     'spider',
-    'mantis'
+    'mantis',
+    'hornet'
 ]
 
 //for(let i = 0; i < bugs.length; i++){
@@ -43,4 +44,40 @@ function list(bug, index, array){
 
 bugs.forEach(list)
 
+var bugLength = bugs.map(bug =>{
+    return bug.length
+})
 
+var firstBugLetter = bugs.map(bug =>{
+    return bug[0]
+})
+
+//console.log(firstBugLetter)
+//
+//var capBug = bugs.map(bug =>{
+//    return.toUpperCase()
+//})
+
+var longBugs = bugs.filter(bug => {
+    return bug.length > 4
+})
+
+//console.log(longBugs)
+
+var notBeetle = bugs.filter(bug =>{
+    return bug !== 'beetle'
+})
+
+console.log(notBeetle)
+
+var aBug = bugs.filter(bug => {
+    return bug.includes('a')
+})
+
+console.log(aBug)
+
+var mantisOrHornet = bugs.filter(bug =>{
+    return bug.includes('mantis') || bug.includes('hornet')
+})
+
+console.log(mantisOrHornet)
